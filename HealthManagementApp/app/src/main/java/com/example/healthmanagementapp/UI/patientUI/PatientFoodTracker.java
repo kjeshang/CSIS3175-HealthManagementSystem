48d13c3bfed0c1f7dc353b41a1cbf971a9a69496
mahdimoghadam <mabbasimoghadam1313@gmail.com>
2021-03-28 00:07:40 -0700

    Creating food tracker activity (leyout)

---
+++ HealthManagementApp/app/src/main/java/com/example/healthmanagementapp/UI/patientUI/PatientFoodTracker.java
@@ -0,0 +1,18 @@
+package com.example.healthmanagementapp.UI.patientUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthmanagementapp.R;
+
+public class PatientFoodTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_food_tracker);
+
+
+    }
+}
