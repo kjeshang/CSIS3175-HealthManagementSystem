9dcc1325426e8ddfa2ec111a8ee4d4e9d1ac8487
mahdimoghadam <mabbasimoghadam1313@gmail.com>
2021-03-28 16:22:02 -0700

    Total Calorie Calculation implemented

--- HealthManagementApp/app/src/main/java/com/example/healthmanagementapp/UI/patientUI/PatientFoodTracker.java
+++ HealthManagementApp/app/src/main/java/com/example/healthmanagementapp/UI/patientUI/PatientFoodTracker.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.healthmanagementapp.R;
 
@@ -18,14 +19,13 @@ public class PatientFoodTracker extends AppCompatActivity implements AdapterView
     Spinner spinner;
     EditText quantity;
     TextView foodList_tv;
-
+    TextView tvCalories;
 
     Button btnSave;
     Button btnHistory;
     Button btnAdd;
-    int itemsCalories = 0;
     String item;
-
+    int itemsCalories = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +39,52 @@ public class PatientFoodTracker extends AppCompatActivity implements AdapterView
 
         foodList_tv = findViewById(R.id.tvFoodList);
         quantity = findViewById(R.id.etQuantity);
-
+        tvCalories = findViewById(R.id.tvCalories);
 
         btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                foodList_tv.append(item + " X " + quantity.getText().toString() + "\n");
+                item = spinner.getSelectedItem().toString();
 
+                switch(item) {
+                    case "Egg":
+                        itemsCalories += (155*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Bread":
+                        itemsCalories += (265*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Tomato":
+                        itemsCalories += (33*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Avocado":
+                        itemsCalories += (160*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Rice":
+                        itemsCalories += (130*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Chicken Breast":
+                        itemsCalories += (165*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Salmon":
+                        itemsCalories += (208*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case  "Carrot":
+                        itemsCalories += (41*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Apple":
+                        itemsCalories += (52*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    case "Orange":
+                        itemsCalories += (47*Integer.parseInt(quantity.getText().toString()));
+                        break;
+                    default:
+                        break;
+
+                }
+
+                foodList_tv.append(item + " X " + quantity.getText().toString() + "\n");
+                tvCalories.setText(String.valueOf(itemsCalories));
 
             }
         });
@@ -54,45 +92,7 @@ public class PatientFoodTracker extends AppCompatActivity implements AdapterView
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        item = parent.getItemAtPosition(position).toString();
-/*
-        switch(item) {
-            case "Egg":
-                itemsCalories =+ (155*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Bread":
-                itemsCalories =+ (265*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Tomato":
-                itemsCalories =+ (33*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Avocado":
-                itemsCalories =+ (160*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Rice":
-                itemsCalories =+ (130*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Chicken Breast":
-                itemsCalories =+ (165*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Salmon":
-                itemsCalories =+ (208*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case  "Carrot":
-                itemsCalories =+ (41*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Apple":
-                itemsCalories =+ (52*Integer.parseInt(quantity.getText().toString()));
-                break;
-            case "Orange":
-                itemsCalories =+ (47*Integer.parseInt(quantity.getText().toString()));
-                break;
-            default:
-                break;
-
-        }
-
-*/
+  
 
     }
 
