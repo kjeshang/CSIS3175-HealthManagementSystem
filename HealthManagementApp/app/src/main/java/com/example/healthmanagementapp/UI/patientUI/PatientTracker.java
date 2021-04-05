251f511e4344bfde2e8365ff1ab3f25f509f3699
mahdimoghadam <mabbasimoghadam1313@gmail.com>
2021-04-04 20:43:05 -0700

    Compeleting Food Tracker Activity

--- HealthManagementApp/app/src/main/java/com/example/healthmanagementapp/UI/patientUI/PatientTracker.java
+++ HealthManagementApp/app/src/main/java/com/example/healthmanagementapp/UI/patientUI/PatientTracker.java
@@ -8,39 +8,47 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.healthmanagementapp.R;
 import com.example.healthmanagementapp.dao.DatabaseHelper;
 import com.example.healthmanagementapp.model.patient.Calories;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 
 public class PatientTracker extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     String patientId;
 
-    Calories calories;
-
-    TextView showList;
+    ListView showList;
+    Button btn_showList;
     TextView selectDate;
     DatePickerDialog.OnDateSetListener mDateSetListener;
 
-    String date;
+    String conDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_tracker);
         selectDate = findViewById(R.id.selectDate_tv);
-        showList = findViewById(R.id.showList_tv);
+
+        showList = findViewById(R.id.showList_lv);
+        btn_showList = findViewById(R.id.showList_btn);
 
         databaseHelper = DatabaseHelper.getInstance(this);
 
         SharedPreferences preference = getSharedPreferences("user", MODE_PRIVATE);
         patientId = preference.getString("patientId", null);
+
 
         selectDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,26 +65,43 @@ public class PatientTracker extends AppCompatActivity {
                         year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
+
             }
         });
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month +1;
-                date = month + "/" + dayOfMonth + "/" + year;
-                selectDate.setText(date);
+                conDate = month + "/" + dayOfMonth + "/" + year;
+                selectDate.setText(conDate);
 
             }
         };
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        btn_showList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        if (databaseHelper.checkIfCalorieExists(date)) {
-            calories = databaseHelper.getCaloriesByDate(date);
+                if (databaseHelper.checkIfCalorieExists(conDate)) {
 
-            showList.setText(calories.display());
+                    for(int i=0; i < databaseHelper.getAllCaloriesByDate(conDate).size(); i++) {
+                        arrayList.add(databaseHelper.getAllCaloriesByDate(conDate).get(i).getFoodList() + "  " +
+                                databaseHelper.getAllCaloriesByDate(conDate).get(i).getDateOfConsumption());
 
-        } else {
-            showList.setText("THERE IS NO PLAN CORRESPONDING TO THE ENTERED DATE");
-        }
+                    }
+
+                    showList.setAdapter(arrayAdapter);
+
+                } else {
+                    Toast.makeText(PatientTracker.this, "Please enter a Date", Toast.LENGTH_LONG).show();
+                }
+            }
+
+
+        });
+
+
 
 
     }
